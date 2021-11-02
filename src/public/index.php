64e3e951115df9f8e70c2da074a204005710422e
Aleksander Jaworski <aleck156@gmail.com>
2021-11-02 08:47:04 +0100

    add autoloader

--- src/public/index.php
+++ src/public/index.php
@@ -2,10 +2,15 @@
 
 declare(strict_types=1);
 
-require_once '../app/PaymentGateway/Stripe/Transaction.php';
-require_once '../app/Notification/Email.php';
-require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
-require_once '../app/PaymentGateway/Paddle/Transaction.php';
+// require_once '../app/PaymentGateway/Stripe/Transaction.php';
+// require_once '../app/Notification/Email.php';
+// require_once '../app/PaymentGateway/Paddle/CustomerProfile.php';
+// require_once '../app/PaymentGateway/Paddle/Transaction.php';
+
+spl_autoload_register(function($class){
+  $path = __DIR__ . '/../'. lcfirst(str_replace('\\', '/', $class).'.php');
+  require $path;
+});
 
 use App\PaymentGateway\Paddle\Transaction;
 
